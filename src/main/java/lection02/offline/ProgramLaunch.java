2533202b7ecbf0c6bd54a9e955dc24c5de6b2a10
Andrey <asapelkin86@yandex.ru>
2022-01-22 13:13:05 +0300

    Домашнее задание к уроку №2.

--- src/main/java/lection02/offline/ProgramLaunch.java
+++ src/main/java/lection02/offline/ProgramLaunch.java
@@ -1,7 +1,82 @@
 package lection02.offline;
 
+
 public class ProgramLaunch {
+
     public static void main(String[] args) {
 
+        String[][] arr1 = new String[][]{
+                {"4", "8", "14", "8"},
+                {"9", "8", "42", "54"},
+                {"98", "7", "4", "tr"},
+                {"549", "1001", "-47", "-800"}};
+
+        String[][] arr2 = new String[][]{
+                {"4", "8", "14", "8", "13"},
+                {"9", "8", "42", "54"},
+                {"98", "7", "4", "tr", "24"},
+                {"549", "1001", "-47", "-800"}};
+
+        String[][] arr3 = new String[][]{
+                {"4", "8", "14", "8"},
+                {"9", "8", "42", "54"},
+                {"98", "7", "4", "5"},
+                {"549", "1001", "-47", "-800"}};
+
+        String[][] arr4 = new String[][]{
+                {"4", "8", "14", "8"},
+                {"9", "8", "42", "54"},
+                {"98", "7", "4", "5"},
+                {"549", "1001", "-47", "#231dd"}};
+
+        String[][] arr5 = new String[][]{
+                {"4", "8", "14", "8"},
+                {"9", "8", "42", "54"},
+                {"98", "7", "4", "5"},
+                {"s", "1001", "-47", "-800"}};
+
+        String[][] arr6 = new String[][]{
+                {"1", "1", "1", "1"},
+                {"1", "1", "1", "1"},
+                {"1", "1", "1", "1"},
+                {"1", "1", "1", "-1"}};
+
+        try {
+            WorkingWithAnArray.sumOfArrayElements(arr1, "arr1");
+        } catch (MyArrayDataException | MyArraySizeException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            WorkingWithAnArray.sumOfArrayElements(arr2, "arr2");
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            WorkingWithAnArray.sumOfArrayElements(arr3, "arr3");
+        } catch (MyArrayDataException | MyArraySizeException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            WorkingWithAnArray.sumOfArrayElements(arr4, "arr4");
+        } catch (MyArrayDataException | MyArraySizeException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            WorkingWithAnArray.sumOfArrayElements(arr5, "arr5");
+        } catch (MyArrayDataException | MyArraySizeException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            WorkingWithAnArray.sumOfArrayElements(arr6, "arr6");
+        } catch (MyArrayDataException | MyArraySizeException e) {
+            e.printStackTrace();
+        }
+
     }
+
 }
--- src/main/java/lection02/offline/WorkingWithAnArray.java
+++ src/main/java/lection02/offline/WorkingWithAnArray.java
@@ -1,4 +1,32 @@
 package lection02.offline;
 
 public class WorkingWithAnArray {
+
+    private static final int ALLOWE_ARRAY_LENGTH = 4;
+
+    public static int sumOfArrayElements(String[][] sourceArray, String sourceArrayName) {
+
+        if (sourceArray == null || sourceArrayName == null) throw new NullPointerException();
+
+        if (sourceArray.length != ALLOWE_ARRAY_LENGTH || sourceArray[0].length != ALLOWE_ARRAY_LENGTH) {
+            throw new MyArraySizeException(String.format("У массива [%s] Количество строк или столбцов превышает [%d]",
+                    sourceArrayName, ALLOWE_ARRAY_LENGTH));
+        }
+
+        int resultSum = 0;
+
+        for (int i = 0; i < ALLOWE_ARRAY_LENGTH; i++) {
+            for (int j = 0; j < ALLOWE_ARRAY_LENGTH; j++) {
+
+                try {
+                    resultSum += Integer.parseInt(sourceArray[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(String.format("У массива [%s] данные из ячейки [%d][%d] невозможно преобразовать в число",
+                            sourceArrayName, i + 1, j + 1));
+                }
+            }
+        }
+        System.out.printf("Сумма элементов массива [%s] равна [%d]%n", sourceArrayName, resultSum);
+        return resultSum;
+    }
 }
